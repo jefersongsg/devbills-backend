05d8b044326d6353afed56c908ee87e5cfefbbbb
jefersongsg <jefersonsilva5525@gmail.com>
2025-05-10 13:27:07 -0300

    conctado no db tinha um erro na URL resolvido e as categorias criadas

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import app from './app'
 import dotenv from 'dotenv'
+import { prismaConnect } from './config/prisma'
+import { initializeGlobalCategories } from './services/globalCategories.services'
 dotenv.config()
 
 const PORT = Number(process.env.PORT || 3000)
 
 const startServer = async () => {
   try {
+    await prismaConnect();
+
+    await initializeGlobalCategories();
+
     await app.listen({ port: PORT }).then(() => {
       console.log(`Server listening on port ${PORT}`)
     })
